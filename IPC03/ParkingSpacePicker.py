229f9c9ae9fe22250a9a5b75ef09aa0fdccafa57
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-17 12:46:34 +0700

    Changes to be committed: 	new file:   IPC03/.idea/.gitignore 	new file:   IPC03/.idea/IPC03.iml 	new file:   IPC03/.idea/inspectionProfiles/Project_Default.xml 	new file:   IPC03/.idea/inspectionProfiles/profiles_settings.xml 	new file:   IPC03/.idea/misc.xml 	new file:   IPC03/.idea/modules.xml 	new file:   IPC03/.idea/other.xml 	new file:   IPC03/ParkingSpacePicker.py 	new file:   IPC03/TniPark.mp4 	new file:   IPC03/TniParkImg.jpg 	new file:   IPC03/TniParkImg.png 	new file:   IPC03/TniParkPos 	new file:   IPC03/TniPark_M.mp4 	new file:   IPC03/TniPark_N.mp4 	new file:   IPC03/main.py

---
+++ IPC03/ParkingSpacePicker.py
@@ -0,0 +1,33 @@
+import cv2
+import pickle
+
+width, height = 30, 20
+
+try:
+    with open('TniParkPos', 'rb') as f:
+        posList = pickle.load(f)
+except:
+    posList = []
+
+
+def mouseClick(events, x, y, flags, params):
+    if events == cv2.EVENT_LBUTTONDOWN:
+        posList.append((x, y))
+    if events == cv2.EVENT_RBUTTONDOWN:
+        for i, pos in enumerate(posList):
+            x1, y1 = pos
+            if x1 < x < x1 + width and y1 < y < y1 + height:
+                posList.pop(i)
+
+    with open('TniParkPos', 'wb') as f:
+        pickle.dump(posList, f)
+
+
+while True:
+    img = cv2.imread('TniParkImg.jpg')
+    for pos in posList:
+        cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
+
+    cv2.imshow("Tni Parking AI_03", img)
+    cv2.setMouseCallback("Tni Parking AI_03", mouseClick)
+    cv2.waitKey(1)
