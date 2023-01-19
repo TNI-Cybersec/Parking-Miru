65176459e8500f65732bd9590a1fa19d4cc77639
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-19 08:06:15 +0700

    Changes to be committed: 	new file:   IPC02/IPC02.py 	modified:   IPC02/ParkingSpacePicker.py 	new file:   IPC02/Parking_Zone_B.txt

--- IPC02/ParkingSpacePicker.py
+++ IPC02/ParkingSpacePicker.py
@@ -28,6 +28,6 @@ while True:
     for pos in posList:
         cv2.rectangle(img, pos, (pos[0] + width, pos[1] + height), (255, 0, 255), 2)
 
-    cv2.imshow("Tni Parking AI_02", img)
-    cv2.setMouseCallback("Tni Parking AI_02", mouseClick)
+    cv2.imshow("Tni Parking Space_02", img)
+    cv2.setMouseCallback("Tni Parking Space_02", mouseClick)
     cv2.waitKey(1)
