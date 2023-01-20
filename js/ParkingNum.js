1c4d345d9617327958c70402dc4085fe36917963
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2023-01-20 18:24:16 +0700

    Format with Prettier

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -1,35 +1,35 @@
 function updateHTML01() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("content01").innerHTML = this.responseText;
-          }
-        };
-        xhttp.open("GET", "FileText/Parking_Zone_A.txt", true);
-        xhttp.send();
-      }
-      setInterval(updateHTML01, 1);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content01").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "FileText/Parking_Zone_A.txt", true);
+  xhttp.send();
+}
+setInterval(updateHTML01, 1);
 
-      function updateHTML02() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("content02").innerHTML = this.responseText;
-          }
-        };
-        xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
-        xhttp.send();
-      }
-      setInterval(updateHTML02, 1);
+function updateHTML02() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content02").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
+  xhttp.send();
+}
+setInterval(updateHTML02, 1);
 
-      function updateHTML03() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("content03").innerHTML = this.responseText;
-          }
-        };
-        xhttp.open("GET", "FileText/Parking_Zone_C.txt", true);
-        xhttp.send();
-      }
-      setInterval(updateHTML03, 1);
+function updateHTML03() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("content03").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "FileText/Parking_Zone_C.txt", true);
+  xhttp.send();
+}
+setInterval(updateHTML03, 1);
--- js/clockEN.js
+++ js/clockEN.js
@@ -1,5 +1,9 @@
 function refreshTime() {
-  var dateString = new Date().toLocaleString("en-GB", {dateStyle: "short",timeStyle: "short",timeZone: "Asia/Bangkok"});
+  var dateString = new Date().toLocaleString("en-GB", {
+    dateStyle: "short",
+    timeStyle: "short",
+    timeZone: "Asia/Bangkok",
+  });
   var formattedString = dateString.replace(", ", " ");
   document.getElementById("date").innerHTML = formattedString;
 }
