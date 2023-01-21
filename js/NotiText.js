8fe39c1c1db961d6870e7c8e94d189075edb2f50
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-22 04:20:56 +0700

    Changes to be committed:

--- js/NotiText.js
+++ js/NotiText.js
@@ -3,10 +3,10 @@ setInterval(function() {
     var hours = currentTime.getHours();
 
     if (hours >= 8 && hours < 15) {
-      document.getElementById("text").innerHTML = "⛔ห้ามรถนักศึกษาเข้าจอด⛔";
+      document.getElementById("text").innerHTML = "⛔ ห้ามรถนักศึกษาเข้าจอด";
     }
     else {
-      document.getElementById("text").innerHTML = "🆗รถนักศึกษาเข้ามาจอดได้🆗";
+      document.getElementById("text").innerHTML = "🆗 รถนักศึกษาเข้ามาจอดได้";
     }
-}, 10);
+}, 1);
   
