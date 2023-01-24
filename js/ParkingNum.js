001344e727214a24a996004ea8b0766dba510147
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-24 22:32:26 +0700

    Changes to be committed: 	modified:   js/ParkingNum.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -1,11 +1,21 @@
-function updateHTML01() {
+function updateText() {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("content01").innerHTML = this.responseText;
-    }
+  xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+          var a = parseInt(this.responseText);
+          var xhttp2 = new XMLHttpRequest();
+          xhttp2.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                  var b = parseInt(this.responseText);
+                  var c = a + b;
+                  document.getElementById("content").innerHTML = c;
+              }
+          };
+          xhttp2.open("GET", "FileText/Parking_Zone_C.txt", true);
+          xhttp2.send();
+      }
   };
   xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
   xhttp.send();
 }
-setInterval(updateHTML01, 10);
+setInterval(updateText, 10000);
