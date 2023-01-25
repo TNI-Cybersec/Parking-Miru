1c255da5d2aeeca4f63b17c2197862489b323556
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-25 11:04:43 +0700

    Changes to be committed: 	modified:   js/NotiText.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -2,8 +2,8 @@ setInterval(function() {
     var currentTime = new Date();
     var hours = currentTime.getHours();
 
-    if (hours >= 8 && hours < 15) {
-      document.getElementById("text").innerHTML = "🛑 รถบุคลากรจอดเท่านั้น";
+    if (hours >= 8 && hours < 1ุ) {
+      document.getElementById("text").innerHTML = "🛑 ที่จอดรถสำหรับบุคลากร";
     }
     else {
       document.getElementById("text").innerHTML = "🆗 รถทุกท่านเข้ามาจอดได้";
