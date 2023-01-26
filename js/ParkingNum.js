9b8323f895c95c95b4fac645a8ffb603aad2b3b0
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-26 18:14:39 +0700

    Changes to be committed: 	modified:   js/ParkingNum.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -20,12 +20,26 @@ function updateText() {
 }
 setInterval(updateText, 1000);
 
-function getData() {
-    fetch('/data')
+var value1;
+var value2;
+function getData1() {
+    fetch('/data1')
         .then(response => response.json())
         .then(data => {
-            document.getElementById("contentpy").innerHTML = data.value;
+            value1 = data.value1;
+            document.getElementById("value1").innerHTML = data.value1;
+            document.getElementById("result").innerHTML = value1 + value2;
+        });
+}
+function getData2() {
+    fetch('/data2')
+        .then(response => response.json())
+        .then(data => {
+            value2 = data.value2;
+            document.getElementById("value2").innerHTML = data.value2;
+            document.getElementById("result").innerHTML = value1 + value2;
         });
 }
 
-setInterval(getData, 1000);
+setInterval(getData1, 1000);
+setInterval(getData2, 1000);
