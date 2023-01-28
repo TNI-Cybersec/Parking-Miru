60bd9ea633f9a68de126f3bad60afccd90c776ea
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-28 18:23:28 +0700

    Changes to be committed: 	modified:   js/NotiText.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -3,10 +3,10 @@ setInterval(function() {
     var hours = currentTime.getHours();
 
     if (hours >= 8 && hours < 16) {
-      document.getElementById("text").innerHTML = "ที่จอดรถบุคลากรและอาจารย์";
+      document.getElementById("text").innerHTML = "🅿 ที่จอดรถบุคลากรและอาจารย์";
     }
     else {
-      document.getElementById("text").innerHTML = "ที่จอดรถบุคคลทั่วไป";
+      document.getElementById("text").innerHTML = "🅿 ที่จอดรถบุคคลทั่วไป";
     }
 }, 10);
   
