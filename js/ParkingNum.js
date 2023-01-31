60b52488a665c6b40ba64d8f0b43e38ec8d4bdca
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-01-31 12:12:35 +0700

    Changes to be committed: 	modified:   js/ParkingNum.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -1,24 +1,30 @@
 function updateText() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          var a = parseInt(this.responseText);
-          var xhttp2 = new XMLHttpRequest();
-          xhttp2.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                  var b = parseInt(this.responseText);
-                  var c = a + b;
-                  document.getElementById("content").innerHTML = c;
-              }
-          };
-          xhttp2.open("GET", "FileText/Parking_Zone_C.txt", true);
-          xhttp2.send();
-      }
-  };
-  xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
-  xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var a = parseInt(this.responseText);
+            if (isNaN(a)) {
+                a = 0;
+            }
+            var xhttp2 = new XMLHttpRequest();
+            xhttp2.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    var b = parseInt(this.responseText);
+                    if (isNaN(b)) {
+                        b = 0;
+                    }
+                    var c = a + b;
+                    document.getElementById("content").innerHTML = c;
+                }
+            };
+            xhttp2.open("GET", "FileText/Parking_Zone_C.txt", true);
+            xhttp2.send();
+        }
+    };
+    xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
+    xhttp.send();
 }
-setInterval(updateText, 1000);
+setInterval(updateText, 1000);  
 
 var value1;
 var value2;
