226d62ab40b6f8168a11c7d6c31a5e62c0c1142e
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-03 02:14:51 +0700

    Changes to be committed: 	modified:   js/NotiText.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -5,17 +5,29 @@ setInterval(function() {
   var seconds = currentTime.getSeconds();
 
   if (hours >= 8 && hours < 16) {
-    if (seconds <= 20) {
+    if (seconds <= 10) {
       document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
-    } else if (seconds > 20 && seconds <= 40) {
+    } else if (seconds > 10 && seconds <= 20) {
+      document.getElementById("text").innerHTML = "Parking for Staff";
+    } else if (seconds > 20 && seconds <= 30) {
+      document.getElementById("text").innerHTML = "スタッフ用駐車場";
+    } else if (seconds > 30 && seconds <= 40) {
+      document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
+    } else if (seconds > 40 && seconds <= 50) {
       document.getElementById("text").innerHTML = "Parking for Staff";
     } else {
       document.getElementById("text").innerHTML = "スタッフ用駐車場";
     }
   } else {
-    if (seconds <= 20) {
+    if (seconds <= 10) {
       document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
-    } else if (seconds > 20 && seconds <= 40) {
+    } else if (seconds > 10 && seconds <= 20) {
+      document.getElementById("text").innerHTML = "Guest Parking Available";
+    } else if (seconds > 20 && seconds <= 30) {
+      document.getElementById("text").innerHTML = "ゲストは駐車できます";
+    } else if (seconds > 30 && seconds <= 40) {
+      document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
+    } else if (seconds > 40 && seconds <= 50) {
       document.getElementById("text").innerHTML = "Guest Parking Available";
     } else {
       document.getElementById("text").innerHTML = "ゲストは駐車できます";
