4ce381aac64398bd1a074492309b32dbd454770e
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-02 12:14:35 +0700

    Changes to be committed: 	modified:   js/NotiText.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -1,11 +1,19 @@
 setInterval(function() {
   var currentTime = new Date();
   var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
 
   if (hours >= 8 && hours < 16) {
-    document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
-  }
-  else {
-    document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
+    if (minutes % 2 === 0) {
+      document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
+    } else {
+      document.getElementById("text").innerHTML = "Parking for Staff";
+    }
+  } else {
+    if (minutes % 2 === 0) {
+      document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
+    } else {
+      document.getElementById("text").innerHTML = "Public Parking Available";
+    }
   }
 }, 10);
