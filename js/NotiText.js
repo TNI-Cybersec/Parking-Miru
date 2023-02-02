960937944de8eb9076fc224e8c8e08fcf5ad6600
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-03 02:34:36 +0700

    Changes to be committed: Add noti text 	modified:   js/NotiText.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -4,33 +4,67 @@ setInterval(function() {
   var minutes = currentTime.getMinutes();
   var seconds = currentTime.getSeconds();
 
+//Set Parking for Staff Noti Text.
+  var Aa = "ที่จอดรถสำหรับบุคลากร";
+  var Ab = "Parking for Staff";
+  var Ac = "スタッフ用駐車場";
+//Set Parking for Guest Noti Text.
+  var Ba = "บุคคลทั่วไปสามารถจอดได้";
+  var Bb = "Guest Parking Available";
+  var Bc = "ゲストは駐車できます";
+
+
   if (hours >= 8 && hours < 16) {
-    if (seconds <= 10) {
-      document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
-    } else if (seconds > 10 && seconds <= 20) {
-      document.getElementById("text").innerHTML = "Parking for Staff";
-    } else if (seconds > 20 && seconds <= 30) {
-      document.getElementById("text").innerHTML = "スタッフ用駐車場";
-    } else if (seconds > 30 && seconds <= 40) {
-      document.getElementById("text").innerHTML = "ที่จอดรถสำหรับบุคลากร";
-    } else if (seconds > 40 && seconds <= 50) {
-      document.getElementById("text").innerHTML = "Parking for Staff";
+    if (seconds <= 5) {
+      document.getElementById("text").innerHTML = Aa;
+    } else if (seconds > 5 && seconds <= 10) {
+      document.getElementById("text").innerHTML = Ab;
+    } else if (seconds > 10 && seconds <= 15) {
+      document.getElementById("text").innerHTML = Ac;
+    } else if (seconds > 15 && seconds <= 20) {
+      document.getElementById("text").innerHTML = Aa;
+    } else if (seconds > 20 && seconds <= 25) {
+      document.getElementById("text").innerHTML = Ab;
+    } else if (seconds > 25 && seconds <= 30) {
+      document.getElementById("text").innerHTML = Ac;
+    } else if (seconds > 30 && seconds <= 35) {
+      document.getElementById("text").innerHTML = Aa;
+    } else if (seconds > 35 && seconds <= 40) {
+      document.getElementById("text").innerHTML = Ab;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Ac;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Aa;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Ab;
     } else {
-      document.getElementById("text").innerHTML = "スタッフ用駐車場";
+      document.getElementById("text").innerHTML = Ac;
     }
   } else {
-    if (seconds <= 10) {
-      document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
-    } else if (seconds > 10 && seconds <= 20) {
-      document.getElementById("text").innerHTML = "Guest Parking Available";
-    } else if (seconds > 20 && seconds <= 30) {
-      document.getElementById("text").innerHTML = "ゲストは駐車できます";
-    } else if (seconds > 30 && seconds <= 40) {
-      document.getElementById("text").innerHTML = "บุคคลทั่วไปสามารถจอดได้";
-    } else if (seconds > 40 && seconds <= 50) {
-      document.getElementById("text").innerHTML = "Guest Parking Available";
+    if (seconds <= 5) {
+      document.getElementById("text").innerHTML = Ba;
+    } else if (seconds > 5 && seconds <= 10) {
+      document.getElementById("text").innerHTML = Bb;
+    } else if (seconds > 10 && seconds <= 15) {
+      document.getElementById("text").innerHTML = Bc;
+    } else if (seconds > 15 && seconds <= 20) {
+      document.getElementById("text").innerHTML = Ba;
+    } else if (seconds > 20 && seconds <= 25) {
+      document.getElementById("text").innerHTML = Bb;
+    } else if (seconds > 25 && seconds <= 30) {
+      document.getElementById("text").innerHTML = Bc;
+    } else if (seconds > 30 && seconds <= 35) {
+      document.getElementById("text").innerHTML = Ba;
+    } else if (seconds > 35 && seconds <= 40) {
+      document.getElementById("text").innerHTML = Bb;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Bc;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Ba;
+    } else if (seconds > 40 && seconds <= 45) {
+      document.getElementById("text").innerHTML = Bb;
     } else {
-      document.getElementById("text").innerHTML = "ゲストは駐車できます";
+      document.getElementById("text").innerHTML = Bc;
     }
   }
 }, 10);
