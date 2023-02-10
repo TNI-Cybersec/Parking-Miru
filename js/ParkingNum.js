91dacddbecb9b0940d6890c569ba7b03103e0fcd
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-10 21:41:55 +0700

    Changes to be committed: AJAX 	modified:   FileText/Parking_Zone_B.txt 	modified:   FileText/Parking_Zone_C.txt 	modified:   index.html 	modified:   js/ParkingNum.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -1,3 +1,4 @@
+/*
 function updateText() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -25,70 +26,34 @@ function updateText() {
   xhttp.send();
 }
 setInterval(updateText, 1000);
+*/
 
-
-function updateTextM() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var a = parseFloat(this.responseText);
+function updateText() {
+  $.ajax({
+    url: "FileText/Parking_Zone_B.txt",
+    success: function (response) {
+      var a = parseFloat(response);
       if (isNaN(a)) {
         a = 0;
       }
-      var xhttp2 = new XMLHttpRequest();
-      xhttp2.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          var b = parseFloat(this.responseText);
+      $.ajax({
+        url: "FileText/Parking_Zone_C.txt",
+        success: function (response) {
+          var b = parseFloat(response);
           if (isNaN(b)) {
             b = 0;
           }
           var c = a + b;
-          document.getElementById("contentM1").innerHTML = c;
+          $("#content").html(c);
         }
-      };
-      xhttp2.open("GET", "FileText/Parking_Zone_C.txt", true);
-      xhttp2.send();
+      });
     }
-  };
-  xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
-  xhttp.send();
-}
-setInterval(updateTextM, 1000);
-
-function getParkingSpace() {
-
-}
-
-setInterval(getParkingSpace, 1000);
-
-
-
-let parkingSpace1;
-let parkingSpace2;
-
-function getValues() {
-  fetch("http://localhost:3000/parking_space1")
-  .then(response => response.json())
-  .then(data => {
-    parkingSpace1 = data.parking_space;
-    updateValues();
   });
-
-  fetch("http://localhost:3000/parking_space2")
-    .then(response => response.json())
-    .then(data => {
-      parkingSpace2 = data.parking_space;
-      updateValues();
-    });
-
 }
 
-function updateValues() {
-  document.getElementById("contentM").innerHTML = parkingSpace1 + parkingSpace2;
-
-}
-setInterval(getValues, 1000);
-
+$(document).ready(function () {
+  setInterval(updateText, 2000);
+});
 
 /*
 function getValues() {
