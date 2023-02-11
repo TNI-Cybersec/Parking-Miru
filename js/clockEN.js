e4337d37ea38e9a48e8fc88c3fe6015cbb6a9dec
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-11 18:22:03 +0700

    Changes to be committed: Fix to AJAX 	modified:   js/clockEN.js

--- js/clockEN.js
+++ js/clockEN.js
@@ -1,10 +1,19 @@
 function refreshTime() {
-  var dateString = new Date().toLocaleString("en-GB", {
-    dateStyle: "short",
-    timeStyle: "medium",
-    timeZone: "Asia/Bangkok",
-  });
-  var formattedString = dateString.replace(", ", " ");
-  document.getElementById("date").innerHTML = formattedString;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      var dateString = new Date(response.time).toLocaleString("en-GB", {
+        dateStyle: "short",
+        timeStyle: "medium",
+        timeZone: "Asia/Bangkok",
+      });
+      var formattedString = dateString.replace(", ", " ");
+      document.getElementById("date").innerHTML = formattedString;
+    }
+  };
+  request.open("GET", "/time", true);
+  request.send();
 }
+
 setInterval(refreshTime, 10);
