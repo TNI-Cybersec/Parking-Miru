01090fce6fc0650c533199907bd21ef864f313d3
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-11 20:04:33 +0700

    Changes to be committed:Back To Original 	modified:   js/ParkingNum.js 	modified:   js/clockEN.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -1,3 +1,51 @@
+function updateText() {
+  $.ajax({
+    url: "FileText/Parking_Zone_B.txt",
+    success: function (response) {
+      var a = parseInt(response);
+      if (isNaN(a)) {
+        a = 0;
+      }
+      $.ajax({
+        url: "FileText/Parking_Zone_C.txt",
+        success: function (response) {
+          var b = parseInt(response);
+          if (isNaN(b)) {
+            b = 0;
+          }
+          var c = a + b;
+          $("#content").html(c);
+        }
+      });
+    }
+  });
+}
+
+$(document).ready(function () {
+  setInterval(updateText, 1000);
+});
+
+
+/*
+function getValues() {
+    let value1;
+    let value2;
+    fetch("http://localhost:5000/values1")
+      .then(response => response.json())
+      .then(data => {
+        value1 = data.value1;
+      });
+  
+    fetch("http://localhost:5000/values2")
+      .then(response => response.json())
+      .then(data => {
+        value2 = data.value2;
+      });
+    document.getElementById("contentGet").innerHTML = value1 + value2;
+}
+setInterval(getValues, 1000);
+*/
+
 /*
 function updateText() {
   var xhttp = new XMLHttpRequest();
@@ -28,50 +76,3 @@ function updateText() {
 setInterval(updateText, 1000); 
 */
 
-
-function updateText() {
-  $.ajax({
-    url: "FileText/Parking_Zone_B.txt",
-    success: function (response) {
-      var a = parseInt(response);
-      if (isNaN(a)) {
-        a = 0;
-      }
-      $.ajax({
-        url: "FileText/Parking_Zone_C.txt",
-        success: function (response) {
-          var b = parseInt(response);
-          if (isNaN(b)) {
-            b = 0;
-          }
-          var c = a + b;
-          $("#content").html(c);
-        }
-      });
-    }
-  });
-}
-
-$(document).ready(function () {
-  setInterval(updateText, 1000);
-});
-
-/*
-function getValues() {
-    let value1;
-    let value2;
-    fetch("http://localhost:5000/values1")
-      .then(response => response.json())
-      .then(data => {
-        value1 = data.value1;
-      });
-  
-    fetch("http://localhost:5000/values2")
-      .then(response => response.json())
-      .then(data => {
-        value2 = data.value2;
-      });
-    document.getElementById("contentGet").innerHTML = value1 + value2;
-}
-setInterval(getValues, 1000);
-*/
--- js/clockEN.js
+++ js/clockEN.js
@@ -1,19 +1,10 @@
 function refreshTime() {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      var response = JSON.parse(this.responseText);
-      var dateString = new Date(response.time).toLocaleString("en-GB", {
-        dateStyle: "short",
-        timeStyle: "medium",
-        timeZone: "Asia/Bangkok",
-      });
-      var formattedString = dateString.replace(", ", " ");
-      document.getElementById("date").innerHTML = formattedString;
-    }
-  };
-  request.open("GET", "/time", true);
-  request.send();
+  var dateString = new Date().toLocaleString("en-GB", {
+    dateStyle: "short",
+    timeStyle: "medium",
+    timeZone: "Asia/Bangkok",
+  });
+  var formattedString = dateString.replace(", ", " ");
+  document.getElementById("date").innerHTML = formattedString;
 }
-
 setInterval(refreshTime, 10);
