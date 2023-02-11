8e61b951fd9460c01cacdc9a76d118bd63a1a70d
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-11 20:14:30 +0700

    Changes to be committed: Now in Array 	modified:   js/NotiText.js 	modified:   js/Titles.js

--- js/NotiText.js
+++ js/NotiText.js
@@ -1,3 +1,22 @@
+setInterval(function() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  // Set parking noti texts
+  const parkingTexts = {
+    staff: ["ที่จอดรถบุคลากร", "Staff Parking", "スタッフ用駐車場"],
+    guest: ["บุคคลทั่วไปจอดได้", "Guest Parking", "ゲスト用駐車場"],
+  };
+  const parkingIndex = Math.floor(seconds / 10) % parkingTexts.staff.length;
+  const parkingNotiText = (hours >= 8 && hours < 16) ? parkingTexts.staff[parkingIndex] : parkingTexts.guest[parkingIndex];
+
+  // Update text element
+  document.getElementById("text").innerHTML = parkingNotiText;
+}, 10);
+
+/*
 setInterval(function () {
   var currentTime = new Date();
   var hours = currentTime.getHours();
@@ -44,3 +63,4 @@ setInterval(function () {
     }
   }
 }, 10);
+*/
--- js/Titles.js
+++ js/Titles.js
@@ -1,3 +1,19 @@
+setInterval(function() {
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  // Set parking noti texts
+  const parkingNotiTexts = ["ที่จอดว่าง", "Avaliable", "空車"];
+  const parkingIndex = Math.floor(seconds / 10) % parkingNotiTexts.length;
+  const parkingNotiText = parkingNotiTexts[parkingIndex];
+
+  // Update title element
+  document.getElementById("title").innerHTML = parkingNotiText;
+}, 10);
+
+/*
 setInterval(function () {
   var currentTime = new Date();
   var hours = currentTime.getHours();
@@ -24,3 +40,4 @@ setInterval(function () {
     document.getElementById("title").innerHTML = JP;
   }
 }, 10);
+*/
