3b8804d46d80a1fcdf105ad4d7da72e453f4f71b
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-02-12 18:40:36 +0700

    Changes to be committed: Fix to Array. 	modified:   js/ParkingNum.js 	modified:   js/Titles.js

--- js/ParkingNum.js
+++ js/ParkingNum.js
@@ -25,7 +25,6 @@ $(document).ready(function () {
   setInterval(updateText, 1000);
 });
 
-
 /*
 function getValues() {
     let value1;
@@ -45,34 +44,3 @@ function getValues() {
 }
 setInterval(getValues, 1000);
 */
-
-/*
-function updateText() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var a = parseFloat(this.responseText);
-      if (isNaN(a)) {
-        a = 0;
-      }
-      var xhttp2 = new XMLHttpRequest();
-      xhttp2.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          var b = parseFloat(this.responseText);
-          if (isNaN(b)) {
-            b = 0;
-          }
-          var c = a + b;
-          document.getElementById("content").innerHTML = c;
-        }
-      };
-      xhttp2.open("GET", "FileText/Parking_Zone_C.txt", true);
-      xhttp2.send();
-    }
-  };
-  xhttp.open("GET", "FileText/Parking_Zone_B.txt", true);
-  xhttp.send();
-}
-setInterval(updateText, 1000); 
-*/
-
--- js/Titles.js
+++ js/Titles.js
@@ -12,32 +12,3 @@ setInterval(function() {
   // Update title element
   document.getElementById("title").innerHTML = parkingNotiText;
 }, 10);
-
-/*
-setInterval(function () {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-
-  //Set Parking for Staff Noti Text.
-  var TH = "ที่จอดว่าง";
-  var EN = "Avaliable";
-  var JP = "空車";
-  //Set Parking for Guest Noti Text.
-
-  if (seconds <= 10) {
-    document.getElementById("title").innerHTML = TH;
-  } else if (seconds > 10 && seconds <= 20) {
-    document.getElementById("title").innerHTML = EN;
-  } else if (seconds > 20 && seconds <= 30) {
-    document.getElementById("title").innerHTML = JP;
-  } else if (seconds > 30 && seconds <= 40) {
-    document.getElementById("title").innerHTML = TH;
-  } else if (seconds > 40 && seconds <= 50) {
-    document.getElementById("title").innerHTML = EN;
-  } else {
-    document.getElementById("title").innerHTML = JP;
-  }
-}, 10);
-*/
