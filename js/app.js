d9ec3ab6b1325cc3c3770898406591bb64d95367
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2023-03-01 00:20:33 +0700

    Changes to be committed: Fix app.js 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+/* 
+Copyright (c) 2023 Parking Miru Web Engine By Karin Vitoonkijvanit
+
+*** Unauthorized modification of the files in the Parking Miru Web Engine 
+section is not responsible for any damage or errors that will occur and 
+constitutes a violation of the of the creators of the Web Engine. *** 
+*/
+
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
