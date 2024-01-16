f9fa9ae7db525394263c8e0a3063ca391e948df8
Karinzaa! <25452435+karinzaa@users.noreply.github.com>
2024-01-16 12:10:09 +0700

    Changes to be committed:Update 	modified:   IPC02/main.py

--- IPC02/main.py
+++ IPC02/main.py
@@ -11,11 +11,11 @@ global im
 im = None
 
 # Video feed from IPC02 via RTSP (High Resolution)
-# cap = cv2.VideoCapture("rtsp://streaming.planetcloud.cloud:5541/7707ed09-0c72-4429-b2ea-a0cc521773ea/0")
-
+cap = cv2.VideoCapture("rtsp://streaming.planetcloud.cloud:5541/7707ed09-0c72-4429-b2ea-a0cc521773ea/0")
+"""
 # Video feed via File Local (for Fastest Code)
-cap = cv2.VideoCapture('TniPark_M.mp4')
-
+# cap = cv2.VideoCapture('TniPark_M.mp4')
+"""
 # Set the buffer size
 cap.set(cv2.CAP_PROP_BUFFERSIZE, 256)
 
